0c2ca5abadbc30fa086ff735136fedf7bcc322f3
Darek <darekt@abmicro.pl>
2021-11-21 23:10:55 +0100

    Init and add stopwach

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +12,20 @@ namespace Csharp_Recurension
     {
         static void Main(string[] args)
         {
+            const int NUMBER = 3;
+            Stopwatch time = new Stopwatch();
+            time.Start();
+            Console.WriteLine(Strong(NUMBER));
+
+            time.Stop();
+
+            Console.WriteLine(time.ElapsedTicks);
+
+        }
+
+        private static bool Strong(int number)
+        {
+            throw new NotImplementedException();
         }
     }
 }
